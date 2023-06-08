04c14ce594768b1a58e67485f0042484b39e5389
Antoni Zajko <antoni.zajko.1@gmail.com>
2023-06-08 23:04:43 +0200

    Add sample changes

---
+++ test/sample.py
@@ -0,0 +1,10 @@
+print("Hello world")
+
+1 + 2 + 3
+
+
+a = (
+    verersfddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+    + dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+)
+fdvfsd + fsgdffdsg
